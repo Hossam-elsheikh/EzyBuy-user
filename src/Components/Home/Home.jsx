4ecd84c36566e129c734be5f18d14b40e3ff746d
AyaShehata226 <ayashegata@gmail.com>
2023-10-23 11:46:04 +0300

    CircleCat, DropDownCat

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,13 @@
-import Sample from '../Reusable/Sample/Sample'
-import TwoImages from '../Reusable/TwoImages/TwoImages'
-
-// import style from './Home.module.css'
+import Sample from '../Reusable/Sample/Sample';
+import TwoImages from '../Reusable/TwoImages/TwoImages';
+import CircleCat from '../Reusable/CircleCat/CircleCat';
+import DropDownCat from '../Reusable/DropDownCat/DropDownCat';
 export default function Home() {
 
   return <>
   <>
-    <h1>
-    Components Trials
-    </h1>
+    <CircleCat/>
+    <DropDownCat/>
     <TwoImages />
 
   </>
--- src/Components/Reusable/CircleCat/CircleCat.jsx
+++ src/Components/Reusable/CircleCat/CircleCat.jsx
@@ -1,6 +1,19 @@
-// import style from './CircleCat.module.css'
+import classes from "./CircleCat.module.css";
 export default function CircleCat() {
-  return <>
-  <h1>TempleteName</h1>
-  </>
+  return (
+    <>
+      <div
+        className="col-lg-2 col-md- col-sm-4 mb-3"
+        style={{textAlign: "center"}}
+      >
+        <a href="#" className={classes.noDe}>
+          <img
+            src="https://i5.walmartimages.com/dfw/4ff9c6c9-6a9c/k2-_752a86d4-3dad-4536-a126-575ff0fdc50b.v1.jpg"
+            className=" rounded-circle w-100"
+          />
+          <span className="ms-1">Fresh food</span>
+        </a>
+      </div>
+    </>
+  );
 }
