868fd5ffe75fba92f535d4c3dbc0c703a8e573c0
FCICoder <mahmoud.aadel1998@gmail.com>
2023-10-23 17:07:31 +0300

    test

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,12 +3,19 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Container from '../UI/container/Container'
 import './Layout.css'
+import ProductImgs from "../Reusable/ProductImgs/ProductImgs";
+import ProductDetails from "../Reusable/ProductDetails/ProductDetails";
 export default function Layout() {
   return <>
   <Navbar/>
   <div className="content"> 
   <Container>
   <Outlet/>
+  
+  </Container>
+  <Container>
+  <ProductImgs/>
+  <ProductDetails/>
   </Container>
   <Footer/>
   </div>
--- src/Components/Reusable/ProductDetails/ProductDetails.jsx
+++ src/Components/Reusable/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,25 @@
-// import style from './ProductDetails.module.css'
+import { Link } from "react-router-dom";
+
+import style from './ProductDetails.module.css'
 export default function ProductDetails() {
   return <>
-  <h1>TempleteName</h1>
+  <div>
+    <Link to={'#'}>Apple</Link>
+
+    <h2>
+    Open Box | Apple iPad | 9.7-inch Retina | 128GB | Latest OS | Wi-Fi Only | Bundle: Case, Pre-Installed Tempered Glass,
+     Rapid Charger,
+     Bluetooth/Wireless Airbuds By Certified 2 Day Express
+    </h2>
+ 
+  <li className="list-unstyled"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>
+  <i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i> 
+  <i class="fa fa-star-half" aria-hidden="true"></i> (4.7) <a href=""> 1406 revewis</a>
+  </li>
+<div className="mt-3">
+  <h2 className="text-success fw-bolder">Now $218.00 <a className={`text-decoration-line-through h5 text-dark-emphasis`}>$249.00</a>  
+  </h2>
+</div>
+  </div>
   </>
 }
