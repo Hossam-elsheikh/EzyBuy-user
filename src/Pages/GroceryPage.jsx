f3d372d3c51691b5a6d99df8c7d894132d777afc
AyaShehata226 <ayashegata@gmail.com>
2023-10-24 13:28:54 +0300

    Grocery-part2

--- src/Pages/GroceryPage.jsx
+++ src/Pages/GroceryPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import DropDownCont from "../Components/Reusable/DropDownCont/DropDownCont";
 import CirCatCont from "../Components/Reusable/CirCatCont/CirCatCont";
+import CarouselCont from "../Components/Reusable/CarouselCont/CarouselCont";
+import CardCont from "../Components/Reusable/CardCont/CardCont";
 const GroceryPage = () => {
   return (
     <>
@@ -14,6 +16,15 @@ const GroceryPage = () => {
           <hr/>
           <CirCatCont name="Shop essentials" />
           <hr/>
+          <CarouselCont lgCount={7} mdCount={7} smCount={3} count={7} slideCount={3} headline='Top Deals' desc='Up to 65% off'/>
+          <hr/>
+          <CardCont/>
+          <hr/>
+          <CarouselCont lgCount={7} mdCount={7} smCount={3} count={7} slideCount={3} headline='Top Deals' desc='Up to 65% off'/>
+          <hr/>
+          <CarouselCont lgCount={7} mdCount={7} smCount={3} count={7} slideCount={3} headline='Top Deals' desc='Up to 65% off'/>
+          <hr/>
+          <CardCont/>
           </div>
         </div>
       </div>
