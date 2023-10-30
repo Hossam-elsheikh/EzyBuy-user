4862115fd66709b4686626eef97c769f3250fec8
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-10-30 14:18:06 +0200

    n

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -34,27 +34,27 @@ export default function Home() {
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-9b1a/k2-_46d000b6-4576-43ac-aa78-51a2ea2f0844.v1.jpg?odnHeight=388&odnWidth=385&odnBg=&odnDynImageQuality=70',
-      title:'Halloween Thrills'
+      title:"Devil's in the details!"
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-e703/k2-_04fe4d37-4282-43d9-9224-bdc382ef9ee0.v1.jpg?odnHeight=388',
-      title:'Treat bags & \n more.'
+      title:'Up to 65% off'
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-21d7/k2-_010ce435-9cfa-4871-98d9-b58993dee070.v1.jpg?odnHeight=216&odnWidth=794&odnBg=&odnDynImageQuality=70',
-      title:'Treat bags & \n more.'
+      title:'members get free shipping with no order minimum!'
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-d60f/k2-_05fc03e3-7634-4bf2-9540-408e8c1f572b.v1.jpg?odnHeight=216&odnWidth=385&odnBg=&odnDynImageQuality=70',
-      title:'Treat bags & \n more.'
+      title:'Pumpkin carving from 4$'
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-5a93/k2-_83783cdc-5702-4729-8022-911f62460d6e.v1.jpg?odnHeight=256',
-      title:'Treat bags & \n more.'
+      title:'Wickedly Fun Style'
     },
     {
       imgURL:'https://i5.walmartimages.com/dfw/4ff9c6c9-df55/k2-_61d339bc-a3fd-4167-b2eb-242f58043643.v1.jpg?odnHeight=578',
-      title:'Treat bags & \n more.'
+      title:'Treat your pup all fall'
     },
 
   ];
@@ -72,9 +72,9 @@ export default function Home() {
             <ShopNowCard src={cards[3].imgURL} title={cards[3].title}/>
             <div className="d-flex gap-4">
             <ShopNowCard txtColor='white' src={cards[4].imgURL} title={cards[4].title}/>
-            <ShopNowCard txtColor='white' src={cards[5].imgURL} title={cards[5].title}/>
+            <ShopNowCard  src={cards[5].imgURL} title={cards[5].title}/>
             </div>
-            <ShopNowCard src={cards[6].imgURL} title={cards[6].title}/>
+            <ShopNowCard src={cards[6].imgURL} titleWidth='500px' title={cards[6].title}/>
           </div>
           <div className="d-flex align-items-center flex-column gap-4 col-6 col-md-6 col-xl-3 col-lg-3 ">
             <ShopNowCard src={cards[7].imgURL} title={cards[7].title}/>
--- src/Components/Reusable/ShopNowCard/ShopNowCard.jsx
+++ src/Components/Reusable/ShopNowCard/ShopNowCard.jsx
@@ -5,7 +5,7 @@ export default function ShopNowCard(props) {
   <div className={style.card}>
     <img className='img img-fluid' src={props.src} />
     <div>
-    <h3 style={{color:props.txtColor}}>{props.title}</h3>
+    <h3 style={{color:props.txtColor,width:props.titleWidth}} >{props.title}</h3>
     <Link style={{color:props.txtColor}}>Shop now</Link>
     </div>
   </div>
