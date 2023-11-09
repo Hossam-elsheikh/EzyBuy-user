502ce7df910040c0b7ed4c80592c535381de53c8
FCICoder <mahmoud.aadel1998@gmail.com>
2023-11-09 16:45:21 +0200

    'wshList'

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -1,11 +1,18 @@
-import {createContext, useState} from 'react'
+import {createContext, useEffect, useState} from 'react'
 
 export let LoginContext = createContext();
 
 export default function LoginContextProvider(props){
     
 
-    let [customerToken ,setCustomerToken] = useState('');
+    let [customerToken ,setCustomerToken] = useState(null);
+useEffect(()=>{
+    if(localStorage.getItem('customerToken')){
+        setCustomerToken(localStorage.getItem('customerToken'))
+    }else{
+        setCustomerToken(null)
+    }
+},[])
     
 
     return <LoginContext.Provider value={{customerToken , setCustomerToken}}>
--- src/store/slices/productsSlice.js
+++ src/store/slices/productsSlice.js
@@ -1,23 +1,38 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 export const productsAction = createAsyncThunk('products/all',async(category)=>{
-    const res = await  axios.get(`http://localhost:3333/product/${category}`)
-    // for each category compare fav products 
-    let fav = ['ss','sss']
-        res.data.forEach(element => {
-            ///////////
-        });
-      return res.data
-  })
+    try{
+        const res = await  axios.get(`http://localhost:3333/product/${category}`)
+        // for each category compare fav products 
+        let fav = ['ss','sss']
+            res.data.forEach(element => {
+                ///////////
+            });
+          return res.data
+    }catch(err){
+        console.log(err.message);
+    }
+})
 
 const ProductsSlice = createSlice({
     name:'products',
-    initialState:{products:[]},
+    initialState:{
+        products:[],
+        isLoading:false,
+        error:''
+    },
     extraReducers :(builder)=>{
         builder.addCase(productsAction.fulfilled, (state,action)=>{
             state.products = action.payload
+            state.isLoading = false;
+        }),
+        builder.addCase(productsAction.pending, (state,action)=>{
+            state.isLoading = true;
+        }),
+        builder.addCase(productsAction.rejected, (state,action)=>{
+            state.error = action.payload
+
         })
     }
-
 })
 export default ProductsSlice.reducer
